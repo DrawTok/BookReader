4d82c7700c1441f3881cde4de1f68c8cb5e73464
DrawTok <lth121103@gmail.com>
2023-12-14 22:25:31 +0700

    new upload

---
+++ bookReader/models/Database.js
@@ -0,0 +1,45 @@
+
+const mysql = require("mysql");
+
+class Database {
+    constructor() {
+        this.serverName = "localhost";
+        this.databaseName = "health";
+        this.databaseUser = "root";
+        this.databasePassword = null;
+    }
+
+    async connect() {
+        try {
+            const connection = await mysql.createConnection({
+
+                host: this.serverName,
+                user: this.databaseUser,
+                password: this.databasePassword,
+                database: this.databaseName
+            });
+            return connection;
+        } catch (error) {
+            console.error('Connection error: ', error.message);
+            throw error;
+        }
+    }
+
+    async query(sql, values) {
+        const connection = await this.connect();
+        return new Promise((resolve, reject) => {
+            connection.query(sql, values, (error, results) => {
+                connection.end(); // Close the connection after the query
+                if (error) {
+                    console.error('Query error: ', error.message);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    }
+
+}
+
+module.exports = Database;
