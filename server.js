096a9c2f5ccce5c5d313c5893e2e9312740786a0
DrawTok <lth121103@gmail.com>
2023-12-16 16:02:14 +0700

    upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const https = require('https');
+const http = require('http');
 const port = process.env.PORT | 3000;
 const app = require("./app");
-const server = https.createServer(app);
+const server = http.createServer(app);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
