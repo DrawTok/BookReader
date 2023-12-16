cad809660005f4bb1170ec5573cc764172bf053b
Trần Minh Quang <tmquang0209@gmail.com>
2023-12-16 20:06:56 +0700

    fix create user

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
-const Database = require("./Database")
-const crypto = require('crypto');
+const Database = require("./Database");
+const crypto = require("crypto");
 class User extends Database {
-
     constructor() {
         super();
     }
@@ -10,13 +9,13 @@ class User extends Database {
         let connection;
         try {
             connection = await this.connect();
-            const query = "SELECT email, gullName, birthDay, role FROM users WHERE idUser = ?";
+            const query =
+                "SELECT email, gullName, birthDay, role FROM users WHERE idUser = ?";
             const [results] = await connection.query(query, [idUser]);
 
             if (results.length > 0)
                 return { success: true, userData: results[0] };
-            else
-                return { success: false, message: "User not found" };
+            else return { success: false, message: "User not found" };
         } catch (error) {
             console.error("Error get info user: ", error.message);
             throw error;
@@ -36,19 +35,25 @@ class User extends Database {
             const isEmailExists = await this.isExistsEmail(email);
 
             if (!isEmailExists) {
-                return { success: false, error: 'Account does not exist' };
+                return { success: false, error: "Account does not exist" };
             }
 
-            const [userData] = await connection.query("SELECT * FROM users WHERE email = ?", [email]);
+            const [userData] = await connection.query(
+                "SELECT * FROM users WHERE email = ?",
+                [email]
+            );
 
             const hashedPassword = userData[0].password;
 
-            const inputPasswordHash = await crypto.createHash('sha256').update(password).digest('hex');
+            const inputPasswordHash = await crypto
+                .createHash("sha256")
+                .update(password)
+                .digest("hex");
 
             const isPasswordMatch = inputPasswordHash === hashedPassword;
 
             if (!isPasswordMatch) {
-                return { success: false, error: 'Invalid password' };
+                return { success: false, error: "Invalid password" };
             }
 
             return { success: true, userData: userData[0] };
@@ -66,7 +71,10 @@ class User extends Database {
         let connection;
         try {
             connection = await this.connect();
-            const [emailExists] = await connection.query("SELECT COUNT(email) as count FROM users WHERE email = ?", [email]);
+            const [emailExists] = await connection.query(
+                "SELECT COUNT(email) as count FROM users WHERE email = ?",
+                [email]
+            );
 
             if (emailExists[0].count > 0) {
                 return true;
@@ -87,22 +95,33 @@ class User extends Database {
         try {
             connection = await this.connect();
 
-            const [countEmails] = await connection.query("SELECT COUNT(*) AS count FROM users WHERE email = ?", [email]);
+            const [countEmails] = await connection.query(
+                "SELECT COUNT(*) AS count FROM users WHERE email = ?",
+                [email]
+            );
             if (countEmails[0].count > 0) {
-                return { success: false, error: 'Email already exists...' };
+                return { success: false, error: "Email already exists..." };
             }
 
-            const hashedPassword = await crypto.createHash('sha256').update(password).digest('hex');
+            const hashedPassword = await crypto
+                .createHash("sha256")
+                .update(password)
+                .digest("hex");
 
-            const query = "INSERT INTO users (idUser, nameUser, dob, email, phoneNumber, password) VALUES (null, ?, ?, ?, ?, ?)";
-            const [results] = await connection.query(query, [email, fullName, birthDay, hashedPassword, role]);
+            const query =
+                "INSERT INTO users (fullName, birthDay, email, password) VALUES (?, ?, ?, ?)";
+            const [results] = await connection.query(query, [
+                fullName,
+                birthDay,
+                email,
+                hashedPassword,
+            ]);
 
             if (results.affectedRows > 0) {
                 return { success: true, message: "Successful" };
             } else {
                 return { success: false, message: "Failed to create user..." };
             }
-
         } catch (error) {
             console.error("Cannot create new user: ", error.message);
             throw error;
@@ -117,15 +136,21 @@ class User extends Database {
         let connection;
         try {
             connection = await this.connect();
-            const query = "UPDATE users SET nameUser = ?, dob = ?, email = ?, phoneNumber = ? WHERE idUser = ?";
-            const [results] = await connection.query(query, [email, fullName, birthDay, role, idUser]);
+            const query =
+                "UPDATE users SET fullName = ?, birthDay = ?, email = ?, phoneNumber = ? WHERE idUser = ?";
+            const [results] = await connection.query(query, [
+                email,
+                fullName,
+                birthDay,
+                role,
+                idUser,
+            ]);
 
             if (results.affectedRows > 0) {
                 return { success: true, message: "Successful" };
             } else {
                 return { success: false, message: "Failed to update user..." };
             }
-
         } catch (error) {
             console.error("Cannot update user: ", error.message);
             throw error;
@@ -141,38 +166,51 @@ class User extends Database {
         try {
             connection = await this.connect();
 
-            const queryOldPassword = "SELECT password FROM users WHERE idUser = ?";
-            const [oldPasswordRows] = await connection.query(queryOldPassword, [idUser]);
+            const queryOldPassword =
+                "SELECT password FROM users WHERE idUser = ?";
+            const [oldPasswordRows] = await connection.query(queryOldPassword, [
+                idUser,
+            ]);
             const oldPassword = oldPasswordRows[0]?.password;
 
-            const curPasswordHash = crypto.createHash('sha256').update(curPassword).digest('hex');
+            const curPasswordHash = crypto
+                .createHash("sha256")
+                .update(curPassword)
+                .digest("hex");
 
             const passwordMatch = curPasswordHash === oldPassword;
             if (passwordMatch) {
-                const hashedPassword = await crypto.createHash('sha256').update(newPassword).digest('hex');
-                const queryUpdatePassword = "UPDATE users SET password = ? WHERE idUser = ?";
-                const [results] = await connection.query(queryUpdatePassword, [hashedPassword, idUser]);
+                const hashedPassword = await crypto
+                    .createHash("sha256")
+                    .update(newPassword)
+                    .digest("hex");
+                const queryUpdatePassword =
+                    "UPDATE users SET password = ? WHERE idUser = ?";
+                const [results] = await connection.query(queryUpdatePassword, [
+                    hashedPassword,
+                    idUser,
+                ]);
 
                 if (results.affectedRows > 0) {
                     return { success: true, message: "Successful" };
                 } else {
-                    return { success: false, message: "Failed to update password..." };
+                    return {
+                        success: false,
+                        message: "Failed to update password...",
+                    };
                 }
-
             } else {
                 return { success: false, message: "Password is incorrect..." };
             }
         } catch (error) {
             console.error("Cannot update password: ", error.message);
             throw error;
-        }
-        finally {
+        } finally {
             if (connection) {
                 connection.end();
             }
         }
     }
-
 }
 
 module.exports = User;
