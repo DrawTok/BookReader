73b94d26e78ea6785eba335447e398697e3ed35f
DrawTok <lth121103@gmail.com>
2023-12-17 19:09:41 +0700

    fix bug

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const apiAccount = require("./api/apiAccount");
 const apiCategory = require("./api/apiCategory");
 const apiCategoryFav = require("./api/apiCategoryFavorite")
