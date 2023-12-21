d841cb46eaaf0a2557dbd1a3710a4f6a1b4856a9
DrawTok <lth121103@gmail.com>
2023-12-21 16:07:10 +0700

    edit app

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ app.use(express.json());
 const apiBook = require("./api/apiBook");
 const apiAccount = require("./api/apiAccount");
 const apiCategory = require("./api/apiCategory");
-const apiCategoryFav = require("./api/apiCategoryFavorite");
 const apiLibrary = require("./api/apiLibrary");
 
 app.use('/api/', apiBook);
 app.use('/api/', apiAccount);
 app.use('/api/', apiCategory);
-app.use('/api/', apiCategoryFav);
 app.use('/api/', apiLibrary);
 
 module.exports = app;
