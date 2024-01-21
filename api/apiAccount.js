fa80e7edf7cacfa4c2a57424e4a53569eec2a8ab
Trần Minh Quang <tmquang0209@gmail.com>
2024-01-21 09:33:24 +0700

    Refactor API routes and add changePassword endpoint

--- api/apiAccount.js
+++ api/apiAccount.js
@@ -1,37 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const AccountController = require("../controllers/AccountController");
 
-router.get('/getInfoUser/:idUser', async (req, res) => {
+router.get("/getInfoUser/:idUser", async (req, res) => {
     await AccountController.getInfoUser(req, res);
 });
 
-router.post('/registerUser', async (req, res) => {
+router.post("/registerUser", async (req, res) => {
     await AccountController.registerUser(req, res);
 });
 
-router.post('/updateInfoUser', async (req, res) => {
+router.post("/updateInfoUser", async (req, res) => {
     await AccountController.updateInfoUser(req, res);
 });
 
-router.post('/updatePassword', async (req, res) => {
+router.post("/updatePassword", async (req, res) => {
     await AccountController.updatePassword(req, res);
 });
 
-router.post('/authLogin', async (req, res) => {
+router.post("/authLogin", async (req, res) => {
     await AccountController.authLogin(req, res);
 });
 
-router.post('/forgotPassword/:email', async (req, res) => {
+router.post("/forgotPassword/:email", async (req, res) => {
     await AccountController.forgotPassword(req, res);
 });
 
-router.post('/authOTP', async (req, res) => {
+router.post("/authOTP", async (req, res) => {
     await AccountController.authOTP(req, res);
 });
 
-router.post('/resetPassword', async (req, res) => {
+router.post("/resetPassword", async (req, res) => {
     await AccountController.resetPassword(req, res);
+});
+
+router.post("/changePassword", async (req, res) => {
+    await AccountController.changePassword(req, res);
 });
 
 module.exports = router;
